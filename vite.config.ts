d922154c13a657b016fcabf31badfe3e64e18462
technology-qs <technology@apps.csjhs.ntpc.edu.tw>
2026-08-14 12:11:55 +0800

    update base path

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(() => {
       alias: {
         '@': path.resolve(__dirname, '.'),
       },
+    base: '/qs-elemtenary-facilitiescheck/',
     },
     server: {
       // HMR is disabled in AI Studio via DISABLE_HMR env var.
